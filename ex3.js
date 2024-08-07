bec1127f02f2dab98bdfca41d4d1cb70cbfa3f62
alyssonmill <alyssonmillerdev@gmail.com>
2024-08-07 14:03:05 +0100

    Practing ...

--- ex3.js
+++ ex3.js
@@ -15,10 +15,25 @@
 // Input: 7
 // Output: Odd
 
-let num = prompt("Write a number");
+// let num = prompt("Write a number");
 
-if (num % 2 === 0) {
-    console.log("Even number");
-}else{
-    console.log("Odd number");
-}
+// if (num % 2 === 0) {
+//     console.log("Even number");
+// }else{
+//     console.log("Odd number");
+// }
+
+// Exercise 3: Simple Addition
+// Write a program that asks the user for two numbers and then prints the sum of those numbers.
+
+// Example:
+
+// Input: 3, 5
+// Output: 8
+
+let num1 = Number(prompt("Write a number"));
+let num2 = Number(prompt("Write a number"));
+
+let result = num1 + num2;
+
+alert("The sum is: " + result)
